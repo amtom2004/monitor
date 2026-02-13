41506750d017953d1233e15fe3ca7d1d062afc0b
amtom2004 <amtombcs@gmail.com>
2026-02-13 10:46:45 +0530

    INIT-001 K8s Manifests Added

---
+++ app/app.py
@@ -0,0 +1,38 @@
+import flask from Flask, jsonify
+import os
+import socket
+
+app = Flask(__name__)
+
+VERSION = os.getenv('APP_VERSION', '1.0')
+HOSTNAME = socket.gethostname()
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": f"Hello, from Monitor Version {VERSION}",
+        "hostname": HOSTNAME,
+        "version": VERSION 
+    })
+
+@app.route('/health/live')
+def liveness():
+    return jsonify({
+        "status": "alive"
+    })
+
+@app.route('/health/ready')
+def readiness():
+    return jsonify({
+        "status": "ready"
+    })
+
+@app.route('/version')
+def version():
+    return jsonify({
+        "version": VERSION,
+        "hostname": HOSTNAME
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
